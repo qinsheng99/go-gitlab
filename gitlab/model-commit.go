adc436436b4227b00de207e692cf536dcd38c0fd
zjm <1076115376@qq.com>
2022-10-14 16:43:52 +0800

    fix

--- gitlab/model-commit.go
+++ gitlab/model-commit.go
@@ -26,13 +26,13 @@ func (g *GetCommit) GetCommittedDate() string {
 }
 
 type Actions struct {
-	Action          string `json:"action"`
-	FilePath        string `json:"file_path"`
-	PreviousPath    string `json:"previousPath"`
-	Content         string `json:"content"`
-	Encoding        string `json:"encoding"`
-	LastCommitId    string `json:"last_commit_id"`
-	ExecuteFilemode string `json:"execute_filemode"`
+	Action          string `json:"action" description:"操作create、delete、move、update、chmod" required:"true"`
+	FilePath        string `json:"file_path" description:"文件路径" required:"true"`
+	PreviousPath    string `json:"previousPath" description:"被移动文件的原始完整路径, 适用于move" required:"false"`
+	Content         string `json:"content" description:"文件内容，除了 delete、chmod 和 move 外其他操作必填" required:"false"`
+	Encoding        string `json:"encoding" description:"text 或者 base64。默认值是 text" required:"false"`
+	LastCommitId    string `json:"last_commit_id" description:"最后一个已知的文件提交 ID。仅适用于update、move 和 delete 操作" required:"false"`
+	ExecuteFilemode bool   `json:"execute_filemode" description:"适用于 chmod 操作,添加可执行标志" required:"false"`
 }
 
 type Tree struct {
@@ -58,7 +58,10 @@ type CommitOne struct {
 
 type PostCommit struct {
 	BaseCommit
-	Branch  string    `description:"分支" required:"true"`
-	Message string    `description:"commit信息" required:"true"`
-	Actions []Actions `description:"提交文件" required:"true"`
+	Branch      string    `json:"branch" description:"分支" required:"true"`
+	Message     string    `json:"commit_message" description:"commit信息" required:"true"`
+	StartBranch string    `json:"start_branch" description:"新创建的分支派生自的分支名称" required:"false"`
+	AuthorEmail string    `json:"author_email,omitempty" description:"提交者邮箱" required:"false"`
+	AuthorName  string    `json:"author_name,omitempty" description:"提交者名字" required:"false"`
+	Actions     []Actions `json:"actions" description:"提交文件" required:"true"`
 }
